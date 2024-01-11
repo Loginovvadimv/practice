c6a0b17e168bfda7ca8289489c2b5ab1cb3a89c0
loginovvadimv@gmail.com <loginovvadimv@gmail.com>
2024-01-12 01:17:51 +0300

    add modals tabs forms

---
+++ src/js/modules/forms.js
@@ -0,0 +1,49 @@
+const forms = () => {
+  const form = document.querySelectorAll('.form');
+  const inputs = document.querySelectorAll('input');
+
+  const message = {
+    success: 'success',
+    loading: 'loading...',
+    failure: 'fail post'
+  };
+
+  const postData = async (url, data) => {
+    document.querySelector('.status').textContent = message.loading;
+    let res = await fetch(url, {
+      method: 'POST',
+      body: data
+    });
+    return await res.text();
+  };
+
+  const clearForm = () => {
+    inputs.forEach(item => {
+      item.value = '';
+    })
+  }
+
+  form.forEach(item => {
+    item.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      let statusMessege = document.createElement('div');
+      statusMessege.classList.add('status');
+      item.appendChild(statusMessege);
+
+      const formData = new FormData(item);
+
+      postData('assets/server.php', formData).then(res => {
+        statusMessege.textContent = message.success;
+      }).catch(() => statusMessege.textContent = message.failure).finally(() => {
+        clearForm();
+        setTimeout(() => {
+          statusMessege.remove();
+        }, 5000);
+      });
+
+    });
+  });
+}
+
+export default forms;
